6fc2aca801e875941957a1327e998c15beb0da74
Patrick Cheng <pcheng86@gmail.com>
2023-08-23 02:31:53 -0700

    tooltip

--- src/components/trendStatement.js
+++ src/components/trendStatement.js
@@ -7,7 +7,7 @@ export default function TrendStatement ({ trend }) {
     else if (trend === 'Sideways up' || trend === 'Sideways down') img = '/sideways.png'
     
     return (
-        <div id='trendStatement' style={{opacity: 0, transition: 'opacity 0.5s'}} className='w-full flex flex-grow justify-center mb-8 items-center'>
+        <div id='trendStatement' style={{opacity: 0, transition: 'opacity 0.5s ease'}} className='w-full flex flex-grow justify-center mb-8 items-center'>
             <div style={{color: '#697184'}} className='text-xl pr-3 font-serif italic'>The trend is {trend ? `"${trend}"` : '...'}</div>
             <img src={img} style={{opacity: .6}} className='h-5 translate-y-.5 -translate-x-1' />
         </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,13 @@ export default function Home() {
     <main
       className={`flex h-screen flex-col items-center justify-center text-black`}>
       <m.div initial={{opacity: 0}} animate={{opacity: 1}} transition={{duration:1, ease:'easeOut'}} className='p-8 w-10/12 max-w-4xl h-full flex items-center justify-center flex-col'>
-        <h1 style={{color: '#413F3D'}} className='m-12 text-5xl font-bold italic font-serif drop-shadow-xl'>trendy</h1>
+        <div className='flex items-center justify-center'>
+          <h1 style={{color: '#413F3D'}} className='my-12 mx-1 text-5xl font-bold italic font-serif drop-shadow-xl'>trendy</h1>
+          <div className="tooltip-container">
+            <img id="excl" src='/excl.svg' className='h-8' />
+            <span className='tooltip-text italic font-serif'>the chart is interactive :)</span>
+          </div>
+        </div>
         <ChartAndTrend loading={loading} setLoading={setLoading} ticker={ticker} />
         <NavBar selectedTickers={selectedTickers} currentTicker={ticker} setCurrentTicker={setTicker} setLoading={setLoading} />
       </m.div>
